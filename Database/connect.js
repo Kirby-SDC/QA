a1ff937798a665e70071ec6126309a0c44d9b4e6
Andrew <drew.311.a@gmail.com>
2022-11-03 12:12:31 -0400

    Refactored to use dotenv module
    Added the module to dependencies
    Created a .env to use in the connection config

--- Database/connect.js
+++ Database/connect.js
@@ -1,12 +1,15 @@
+// const path = path.resolve(__dirname, './.env') <--
+require('dotenv').config()
 const Client = require('pg').Client
 
+
 let config = {
-  user:'andrewarsenault',
-  host:'localhost',
-  database: 'postgres',
-  password: '12345',
-  port: 5432,
-  idle_session_timeout: 0
+  user: process.env.USERID,
+  host:process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  idle_session_timeout: process.env.IDLE_SESSION_TIMEOUT
 }
 
 let connectionClient = new Client(config);
--- Database/seed.js
+++ Database/seed.js
@@ -10,15 +10,16 @@ connectionClient.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack)
   }
+  console.log(`connected to '${client.database}' on port ${client.port}`)
   console.log('creating tables and importing csv...')
   connectionClient.query(populate, function(err, result){
     if(err){
-        console.log('error: ', err);
+        console.log('Error when trying to query with sql file: ', err);
         process.exit(1);
     }
     console.log('populate database complete')
     process.exit(0);
   });
-  console.log(`connected to '${client.database}' on port ${client.port}`)
+
 })
 
